8007ab03a8ff7896fad9ac57640c604ca4cb5b41
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2023-05-15 23:19:59 +0300

    CRUD functions added

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,46 @@
 const express = require('express');
+const { request } = require('../../app');
 
 const router = express.Router();
 
+const contacts = require('../../models/contacts');
+
 router.get('/', async (req, res, next) => {
-   res.json({ message: 'test pull request' });
+   const result = await contacts.listContacts();
+   res.json(result);
 });
 
 router.get('/:contactId', async (req, res, next) => {
-   res.json({ message: 'template message' });
+   const { contactId } = req.params;
+   const result = await contacts.getContactById(contactId);
+   if (!result) {
+      return res.status(404).json({ message: 'contact not found' });
+   }
+   res.json(result);
 });
 
 router.post('/', async (req, res, next) => {
-   res.json({ message: 'template message' });
+   const { name, email, phone } = req.body;
+   const newContact = {
+      name,
+      email,
+      phone,
+   };
+   const result = await contacts.addContact(newContact);
+   res.json(result);
 });
 
 router.delete('/:contactId', async (req, res, next) => {
-   res.json({ message: 'template message' });
+   const { contactId } = req.params;
+   const result = await contacts.removeContact(contactId);
+   res.json({ message: `${contactId} was deleted` });
 });
 
 router.put('/:contactId', async (req, res, next) => {
-   res.json({ message: 'template message' });
+   const { contactId } = req.params;
+   const { name, email, phone } = req.body;
+   const result = await contacts.updateContact(contactId, { name, email, phone });
+   res.json({ status: 'success', code: 200, data: { result } });
 });
 
 module.exports = router;
